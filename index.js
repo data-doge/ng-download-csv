57fb396e07998bc4260433d3c90d3b07531be52a
euglazer <eugenestapler@gmail.com>
2016-02-26 19:11:01 -0800

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+(function() {
+  var module = angular.module('ngDownloadCsv', [])
+  module.factory('DownloadCSV', ['$http', function ($http) {
+    return function (params) {
+      return $http({
+        method: 'GET',
+        url: params.url
+      }).success(function (data) {
+        var anchor = angular.element('<a/>')
+        anchor.attr({
+          href: 'data:attachment/csv;charset=utf-8,' + encodeURI(data),
+          target: '_blank',
+          download: params.filename + '.csv'
+        })[0].click()
+      })
+    }
+  }])
+}())
